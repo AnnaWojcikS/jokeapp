e362b55f48081c9112c0e0b728f01ed3ff43a347
Anna Wojcik <aaswieboda@gmail.com>
2019-06-21 10:07:06 +0200

    Adding configuration package.

---
+++ src/main/java/pl/spring/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,17 @@
+package pl.spring.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by A.Wójcik on 21.06.2019.
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
